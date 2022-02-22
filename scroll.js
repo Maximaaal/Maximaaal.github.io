fe70a31cec1147c55782038bc94700eeecc82400
Max Rentmeester <61837530+Maximaaal@users.noreply.github.com>
2022-02-22 17:10:00 +0100

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,21 @@
+const projectTitleTag = document.getElementById("projectTitle");
+const sections = document.querySelectorAll("main section");
+
+document.addEventListener("scroll", function () {
+  const pixels = window.pageYOffset;
+
+  sections.forEach((section) => {
+    if (section.offsetTop - 100 <= pixels) {
+      projectTitleTag.innerHTML = section.getAttribute("project");
+      projectTitleTag.href = section.getAttribute("page");
+      console.log(section.getAttribute("project"));
+    }
+  });
+});
+
+window.addEventListener("load", function () {
+  const dynamicHeight = 400 - document.getElementById("projectTitleBox").offsetHeight
+  document.getElementById("margin").style.height = dynamicHeight + "px";
+  },
+  false
+);
