1cafced70a3386c109383461e161641e31105140
Max Rentmeester <61837530+Maximaaal@users.noreply.github.com>
2024-09-23 10:58:14 +0200

    preloading

--- scroll.js
+++ scroll.js
@@ -1,25 +1,53 @@
 const projectTitleTag = document.getElementById("projectTitle");
 const sections = document.querySelectorAll("main section");
-//const showVidControls = document.getElementsByClassName("vidControls")
+const preloader = document.getElementById("preloader");
+
+function hidePreloader() {
+    preloader.style.display = "none";
+}
+
+function showPreloader() {
+    preloader.style.display = "flex";
+}
+
+function handlePreloader() {
+    if (!localStorage.getItem('notFirstVisit')) {
+        showPreloader();
+        setTimeout(() => {
+            hidePreloader();
+            localStorage.setItem('notFirstVisit', 'true');
+        }, 2000);
+    } else {
+        hidePreloader();
+    }
+}
+
+// Handle preloader on DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    handlePreloader();
+});
+
+// Fallback to ensure preloader is hidden
+window.addEventListener("load", () => {
+    setTimeout(hidePreloader, 100);
+});
 
 document.addEventListener("scroll", function () {
-  const pixels = window.pageYOffset;
-
-  sections.forEach((section) => {
-    if (section.offsetTop - 100 <= pixels) {
-      projectTitleTag.innerHTML = section.getAttribute("project");
-      projectTitleTag.href = section.getAttribute("page");
-      console.log(section.getAttribute("project"));
-    }
-  });
+    const pixels = window.pageYOffset;
+    sections.forEach((section) => {
+        if (section.offsetTop - 100 <= pixels) {
+            projectTitleTag.innerHTML = section.getAttribute("project");
+            projectTitleTag.href = section.getAttribute("page");
+            console.log(section.getAttribute("project"));
+        }
+    });
 });
 
 window.addEventListener("load", function () {
-  const dynamicHeight = 370 - document.getElementById("projectTitleBox").offsetHeight
-  document.getElementById("margin").style.height = dynamicHeight + "px";
-  },
-  false
-);
+    const dynamicHeight = 370 - document.getElementById("projectTitleBox").offsetHeight;
+    document.getElementById("margin").style.height = dynamicHeight + "px";
+}, false);
+
 
 //showVidControls[0].addEventListener("mouseover", mouseOver)
 
